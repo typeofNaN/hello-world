3b80d7a4f819637d7aedc56427420bca33aa37ce
typeofNaN <dmdefine6@gmail.com>
2020-11-27 10:53:19 +0800

    docs: leetcode第一题文档

--- leetcode/1.two-sum/two-sum.js
+++ leetcode/1.two-sum/two-sum.js
@@ -1,10 +1,10 @@
 /**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
  * @description 穷举法，暴力for嵌套循环，时间复杂度 O(n2)
- * @param { number[] } nums
- * @param { number } target
- * @return { number[] }
  */
-const twoSum = (nums, target) => {
+var twoSum = (nums, target) => {
   for (let i = 0; i < nums.length; i++) {
     const dif = target - nums[i]
     // j = i + 1 的目的是减少重复计算和避免两个元素下标相同
@@ -19,12 +19,12 @@ const twoSum = (nums, target) => {
 console.log(twoSum([2, 7, 11, 15], 9))  // [0, 1]
 
 /**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
  * @description 利用数组减少查询时间，时间复杂度 O(n)
- * @param { number[] } nums
- * @param { number } target
- * @return { number[] }
  */
-const twoSum1 = (nums, target) => {
+var twoSum1 = (nums, target) => {
   const temp = []
   for(let i = 0; i < nums.length; i++) {
     const dif = target - nums[i]
@@ -38,12 +38,12 @@ const twoSum1 = (nums, target) => {
 console.log(twoSum1([2, 7, 11, 15], 9))  // [0, 1]
 
 /**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
  * @description 使用hash table方法，时间复杂度 O(n)
- * @param { number[] } nums
- * @param { number } target
- * @return { number[] }
  */
-const twoSum2 = (nums, target) => {
+var twoSum2 = (nums, target) => {
   // 构造哈希表
   const map = new Map()  // 存储方式 {need, index}
 
